6467a995e0ee2e6e196597e3280127785b7c10e0
mawrob <robert@mawrey.com>
2019-10-14 16:22:03 -0400

    Original weatherlevel code
    Original weather and level code

---
+++ src/weather.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "application.h"
+#line 1 "c:/Users/rober/Dropbox/SentientThings/MawreyGithub/weather/src/weather.ino"
+/*
+ * Project weather
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "c:/Users/rober/Dropbox/SentientThings/MawreyGithub/weather/src/weather.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
